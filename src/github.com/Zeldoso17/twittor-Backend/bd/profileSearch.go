6f2cb5583b5eaa07d99b7513bb457c3637b1587d
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-04-13 13:26:45 -0700

    Se crearon archivos para Ver Perfil

---
+++ src/github.com/Zeldoso17/twittor-Backend/bd/profileSearch.go
@@ -0,0 +1,32 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Zeldoso17/twittor-Backend/src/github.com/Zeldoso17/twittor-Backend/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func ProfileSearch(ID string) (models.Usuario, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twittor")
+	col := db.Collection("usuarios")
+
+	var profile models.Usuario
+	objID, _ := primitive.ObjectIDFromHex(ID) // Here we convert the ID to a ObjectID
+
+	condition := bson.M{"_id": objID,}
+
+	err := col.FindOne(ctx, condition).Decode(&profile) // Here we search for the user in the database
+	profile.Password = ""                               // We don't want to send the password to the frontend
+	if err != nil {
+		fmt.Println("Registro no encontrado " + err.Error())
+		return profile, err
+	}
+	return profile, nil
+}
---
+++ src/github.com/Zeldoso17/twittor-Backend/routers/ViewProfile.go
@@ -0,0 +1,27 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/Zeldoso17/twittor-Backend/src/github.com/Zeldoso17/twittor-Backend/bd"
+)
+
+// ViewProfile is a function that allows to obtain the profile of a user
+func ViewProfile(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parámetro ID", http.StatusBadRequest)
+		return
+	}
+
+	profile, err := bd.ProfileSearch(ID)
+	if err != nil {
+		http.Error(w, "Ocurrió un error al intentar buscar el registro "+err.Error(), 400)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(profile)
+
+}
